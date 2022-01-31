6314135c45a114c4a8ea04711809c56c6cb06a31
yupa42 <89157073+yupa42@users.noreply.github.com>
2022-01-31 15:45:37 +0100

    Add files via upload

---
+++ del0.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+if [ $1 -eq 00 ]
+then
+echo 0
+elif [ $1 -eq 01 ]
+then
+echo 1
+elif [ $1 -eq 02 ]
+then
+echo 2
+elif [ $1 -eq 03 ]
+then
+echo 3
+elif [ $1 -eq 04 ]
+then
+echo 4
+elif [ $1 -eq 05 ]
+then
+echo 5
+elif [ $1 -eq 06 ]
+then
+echo 6
+elif [ $1 -eq 07 ]
+then
+echo 7
+elif [ $1 -eq 08 ]
+then
+echo 8
+elif [ $1 -eq 09 ]
+then
+echo 9
+else
+echo $1
+fi
